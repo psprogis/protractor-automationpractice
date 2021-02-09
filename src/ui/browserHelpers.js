ed678d1c521300484f869d5b529fbd745cbe112d
Pavlo Sprogis <pavel.sprogis@gmail.com>
2021-02-09 18:04:41 +0200

    use timeout value from config

--- src/ui/browserHelpers.js
+++ src/ui/browserHelpers.js
@@ -1,5 +1,4 @@
 const { ExpectedConditions: EC } = protractor;
-const DEFAULT_TIMEOUT = 3000;
 
 async function setNonAngularSite() {
     return browser.waitForAngularEnabled(false);
@@ -13,11 +12,11 @@ async function deleteAllCookies() {
     return browser.driver.manage().deleteAllCookies();
 }
 
-async function waitElementVisible({ element, timeout = DEFAULT_TIMEOUT }) {
+async function waitElementVisible({ element, timeout = testConfig.ELEMENT_VISIBLE_TIMEOUT }) {
     return browser.wait(EC.visibilityOf(element), timeout);
 }
 
-async function waitElementNotVisible({ element, timeout = DEFAULT_TIMEOUT }) {
+async function waitElementNotVisible({ element, timeout = testConfig.ELEMENT_VISIBLE_TIMEOUT }) {
     return browser.wait(EC.not(EC.visibilityOf(element)), timeout);
 }
 
